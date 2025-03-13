a9b103aa94d9687ca0ebef7ab0c059d71a33cac3
Nagy Zétény <nagyzeteny6@gmail.com>
2025-03-13 13:34:47 +0100

    init t3 app

---
+++ web/src/server/db/schema.ts
@@ -0,0 +1,25 @@
+import { sql } from "drizzle-orm";
+import {
+  bigint,
+  index,
+  mysqlTableCreator,
+  timestamp,
+  varchar,
+} from "drizzle-orm/mysql-core";
+
+export const createTable = mysqlTableCreator((name) => name);
+
+export const posts = createTable(
+  "post",
+  {
+    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
+    name: varchar("name", { length: 256 }),
+    createdAt: timestamp("created_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at").onUpdateNow(),
+  },
+  (example) => ({
+    nameIndex: index("name_idx").on(example.name),
+  }),
+);
