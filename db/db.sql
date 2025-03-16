ff993781d503631f74da359f6fa5be0fb0aae621
Nagy Zétény <nagyzeteny6@gmail.com>
2025-03-16 19:23:23 +0100

    add table reservation table

---
+++ db/db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS `burger-bar`
+CHARACTER SET utf8
+COLLATE utf8_hungarian_ci;
+
+DROP TABLE IF EXISTS `table-reservation`;
+
+CREATE TABLE `table-reservation` (
+  `id` int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `phone` varchar(256) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `people` int NOT NULL
+);
--- web/src/server/db/schema.ts
+++ web/src/server/db/schema.ts
@@ -1,25 +1,19 @@
-import { sql } from "drizzle-orm";
 import {
-  bigint,
-  index,
+  int,
   mysqlTableCreator,
   timestamp,
   varchar,
+  date,
 } from "drizzle-orm/mysql-core";
 
 export const createTable = mysqlTableCreator((name) => name);
 
-export const posts = createTable(
-  "post",
-  {
-    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at").onUpdateNow(),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  }),
-);
+export const posts = createTable("table-reservation", {
+  id: int("id").primaryKey().autoincrement(),
+  name: varchar("name", { length: 256 }),
+  email: varchar("email", { length: 256 }),
+  phone: varchar("phone", { length: 256 }),
+  date: date("date"),
+  time: timestamp("time"),
+  people: int("people"),
+});
