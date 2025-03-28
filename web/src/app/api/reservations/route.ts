e8f4ad46557aab2a5e30409fb7a7358e5e7edcca
Nagy Zétény <nagyzeteny6@gmail.com>
2025-03-28 19:07:28 +0100

    api create/modify reservation item

--- web/src/app/api/reservations/route.ts
+++ web/src/app/api/reservations/route.ts
@@ -4,9 +4,15 @@ import { tableReservations } from "~/server/db/schema";
 import { deleteFromTable } from "../delete-from-table";
 import { getAllFromTable } from "../get-all-from-table";
 import { updatePartial } from "../updatePartial";
+import { createNew } from "../create-new";
 
 const reservationSchema = z.object({
   id: z.number(),
+  name: z.string(),
+  email: z.string().email(),
+  phone: z
+    .string()
+    .regex(/^[+]?[\s./0-9]*[(]?[0-9]{1,4}[)]?[-\s./0-9]{8,14}$/g),
   date: z.coerce
     .date()
     .transform(
@@ -28,8 +34,16 @@ export async function GET() {
   return getAllFromTable(tableReservations);
 }
 
+export async function POST(request: NextRequest) {
+  return createNew(request, tableReservations, reservationSchema);
+}
+
 export async function PATCH(request: NextRequest) {
-  return updatePartial(request, tableReservations, reservationSchema);
+  return updatePartial(
+    request,
+    tableReservations,
+    reservationSchema.extend({ id: z.number() }),
+  );
 }
 
 export async function DELETE(request: NextRequest) {
