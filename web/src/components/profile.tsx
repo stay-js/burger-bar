a5913b5d3713111ad5ebbbf9a729298765e745ec
Nagy Zétény <nagyzeteny6@gmail.com>
2025-05-27 19:05:29 +0200

    update profile dropdown, remove heroicons

--- web/src/components/profile.tsx
+++ web/src/components/profile.tsx
@@ -1,39 +1,28 @@
-'use client'
+"use client";
 
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
-
-import { useUser, useClerk } from '@clerk/nextjs'
-
-import { useRouter } from 'next/navigation'
-
-import Image from 'next/image'
-
-import Link from 'next/link'
-
-import {
-  ArrowRightEndOnRectangleIcon,
-  Cog6ToothIcon,
-  CalendarIcon ,
-} from '@heroicons/react/24/outline'
-
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useUser, useClerk } from "@clerk/nextjs";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import { Button } from "~/components/ui/button";
+import { CalendarIcon, SettingsIcon, LogOutIcon } from "lucide-react";
 
 export const UserButton = () => {
-  const { isLoaded, user } = useUser()
+  const { isLoaded, user } = useUser();
+  const { signOut, openUserProfile } = useClerk();
 
-  const { signOut, openUserProfile } = useClerk()
+  const router = useRouter();
 
-  const router = useRouter()
+  if (!isLoaded) return null;
+  if (!user?.id) return null;
 
-  if (!isLoaded) return null
-
-  if (!user?.id) return null
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger asChild>
-        <button
-          className="relative h-8 w-8 overflow-hidden rounded-full border border-gray-600 bg-black shadow-sm transition hover:ring-1 hover:ring-gray-600"
+        <Button
+          className="relative h-8 w-8 overflow-hidden rounded-full hover:ring-1 hover:ring-gray-600"
           title="Profil megnyitása"
-          type="button"
         >
           <Image
             src={user.imageUrl}
@@ -41,49 +30,68 @@ export const UserButton = () => {
             fill
             className="object-cover"
           />
-        </button>
+        </Button>
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
         <DropdownMenu.Content
-          sideOffset={10} 
-          align="end" 
-          className="mt-4 w-52 rounded-xl border border-gray-200 bg-white px-4 py-4 text-black drop-shadow-2xl ml-[-10px]"
+          sideOffset={10}
+          align="end"
+          className="mt-4 w-fit min-w-52 rounded-xl border border-gray-200 bg-white px-4 py-4 text-black drop-shadow-2xl"
         >
-          <div className="px-4 py-3">
-            <div className="text-sm font-medium">{user.fullName}</div>
-            <div className="text-xs text-gray-500">{user.primaryEmailAddress?.emailAddress}</div>
+          <div className="flex items-center gap-3 px-4 py-3">
+            <Image
+              src={user.imageUrl}
+              alt="Profilkép"
+              width={36}
+              height={36}
+              className="rounded-full"
+            />
+            <div>
+              <div className="text-sm font-medium">{user.fullName}</div>
+              <div className="text-xs text-gray-500">
+                {user.primaryEmailAddress?.emailAddress}
+              </div>
+            </div>
           </div>
           <DropdownMenu.Separator className="my-1 h-px bg-gray-200" />
           <DropdownMenu.Item asChild>
-            <button
+            <Button
               onClick={() => openUserProfile()}
-              className="flex w-full items-center gap-2 px-4 py-2 text-sm hover:bg-gray-100"
+              variant="ghost"
+              className="flex w-full justify-start gap-2 hover:bg-neutral-100 hover:text-neutral-800"
             >
-              <Cog6ToothIcon className="h-5 w-5 text-gray-500" />
-              Manage account
-            </button>
+              <SettingsIcon className="text-gray-500" />
+              Profil beállítások
+            </Button>
           </DropdownMenu.Item>
           <DropdownMenu.Item asChild>
-            <Link
-              href="/foglalasaim"
-              className="flex w-full items-center gap-2 px-4 py-2 text-sm hover:bg-gray-100"
+            <Button
+              asChild
+              variant="ghost"
+              className="flex w-full justify-start"
             >
-              <CalendarIcon  className="h-5 w-5 text-gray-500" />
-              Foglalásaim
-            </Link>
+              <Link
+                href="/foglalasaim"
+                className="flex w-full justify-start gap-2 hover:bg-neutral-100 hover:text-neutral-800"
+              >
+                <CalendarIcon className="text-gray-500" />
+                Foglalásaim
+              </Link>
+            </Button>
           </DropdownMenu.Item>
           <DropdownMenu.Separator className="my-1 h-px bg-gray-200" />
           <DropdownMenu.Item asChild>
-            <button
-              onClick={() => signOut(() => router.push('/'))}
-              className="flex w-full items-center gap-2 px-4 py-2 text-sm text-red-600 hover:bg-red-50"
+            <Button
+              variant="ghost"
+              className="flex w-full justify-start text-red-600 hover:bg-red-50 hover:text-red-600"
+              onClick={() => signOut(() => router.push("/"))}
             >
-              <ArrowRightEndOnRectangleIcon className="h-5 w-5" />
-              Sign out
-            </button>
+              <LogOutIcon />
+              Kijelentkezés
+            </Button>
           </DropdownMenu.Item>
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
-  )
-}
+  );
+};
